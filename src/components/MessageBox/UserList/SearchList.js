17c5bf4a934fdf2116d70dc3fd367d7476b85251
lzg <lizg@easemob.com>
2021-05-29 13:15:03 +0800

    搜索列表添加默认头像

--- src/components/MessageBox/UserList/SearchList.js
+++ src/components/MessageBox/UserList/SearchList.js
@@ -23,7 +23,7 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute }) => {
                         return (
                             <Flex justifyContent='space-between' alignItems='center' mt='8px' key={key}>
                                 <Flex ml='8px' alignItems='center'>
-                                    <Image src={member[1]} className='msg-img' />
+                                    <Image src={member[1] || 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic4.zhimg.com%2F50%2Fv2-fde5891065510ef51e4c8dc19f6f3aff_hd.jpg&refer=http%3A%2F%2Fpic4.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1624035646&t=52e70633abb73d7e2e0d2bd3f0446505'} className='lsit-user-img' />
                                     <Flex ml='8px'>
                                         {member[2] === roomOwner && <Tag className='tags' ><Text className='tags-txt' ml='4px' mt='1px'>主讲老师</Text></Tag>}
                                         {roomAdmins.includes(member[2]) && <Tag className='tags' ><Text className='tags-txt' ml='4px' mt='1px'>辅导老师</Text></Tag>}
