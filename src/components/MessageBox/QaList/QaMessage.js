65731d6f24c0c00a311be4c78be355952abbb0b5
breeze <1244371708@qq.com>
2021-06-09 17:13:45 +0800

    .feat:提问模块UI <body> 1、 <footer> 关联bugId: 无

--- src/components/MessageBox/QaList/QaMessage.js
+++ src/components/MessageBox/QaList/QaMessage.js
@@ -5,8 +5,6 @@ import { Tag } from 'antd'
 
 // 助教端 提问消息列表
 const QaMessage = (props) => {
-    const teacher = useSelector(state => state.loginInfo.ext);
-    const isTeacher = (Number(teacher) === 1 || Number(teacher) === 3);
     const qaList = useSelector(state => state.messages.qaList) || [];
     const [newUser, setNewUser] = useState([]);
     useEffect(() => {
@@ -20,16 +18,13 @@ const QaMessage = (props) => {
                     let isText = message.type === "txt" || message.contentsType === "TEXT"
                     let isPic = message.type === "img" || message.contentsType === "IMAGE"
                     return (
-                        <div key={index} className='qa-msg' style={{paddingLeft: isTeacher ? 12 : 0}}>
-                            {/* <div>
-                                <Image src={message.ext.avatarUrl}
-                                    className='qa-msg-img'
-                                />
-                            </div> */}
+                        <div key={index} className='qa-msg' >
                             <Flex>
-                                {(message.ext.role === 1) && <Tag className='tags'><Text className='tags-txt' m='4px' mt='1px'>主讲老师</Text></Tag>}
-                                {(message.ext.role === 3) && <Tag className='tags'><Text className='tags-txt' ml='4px' mt='1px'>辅导老师</Text></Tag>}
-                                <Text className='msg-sender' mb='5px'>{message.ext.nickName || message.from}</Text>
+                                {/* 主讲/辅导tag */}
+                                {(message.ext.role === 1 || message.ext.role === 3) && <Tag className='tags'>
+                                    {message.ext.role === 1 && '主讲老师'}{message.ext.role === 3 && '辅导老师'}
+                                </Tag>}
+                                <Text className='msg-sender' mb='5px' color={(message.ext.role === 1 || message.ext.role === 3) && '#0099FF'}>{message.ext.nickName || message.from}</Text>
                             </Flex>
                             {isText && <Text className='msg-text'>{message.msg || message.data}</Text>}
                             {isPic && <Image src={message.url || message.body.url} style={{ width: '180px' }} />}
